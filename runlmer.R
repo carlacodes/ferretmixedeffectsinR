bbe1fb5f544b912d1e8edb76ccd0b79f6e5581e7
carlacodes <carla.griffiths@gmail.com>
2022-10-31 15:10:22 +0000

    adding nakagawa r_2 coeff

--- runlmer.R
+++ runlmer.R
@@ -1,5 +1,26 @@
+## load lme4, JWileymisc, and multilevelTools packages
+## (i.e., "open the 'apps' ") 
+library(lme4)
+#> Loading required package: Matrix
+library(lmerTest)
+#> 
+#> Attaching package: 'lmerTest'
+#> The following object is masked from 'package:lme4':
+#> 
+#>     lmer
+#> The following object is masked from 'package:stats':
+#> 
+#>     step
+library(extraoperators)
+library(JWileymisc)
+library(multilevelTools)
+
 dfuse <- read.csv("dfuse.csv")
 dfcatuse <- read.csv("dfcat_use.csv")
 
 df <- read.csv("df.csv")
 dfcat <- read.csv("dfcat.csv")
+
+modelreg_reduc <- lmer(
+  "realRelReleaseTimes ~ talker*(pitchoftarg)+side + talker*stepval+timeToTarget  + (1|ferret)",
+  data=dfuse, family='gamma')
