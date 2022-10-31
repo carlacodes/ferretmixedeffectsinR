e9052552ac44ed0c0b65e0f8be8969d5cf9b1da9
carlacodes <carla.griffiths@gmail.com>
2022-10-31 15:32:36 +0000

    comparing null models using anova to decide random effects

--- runlmer.R
+++ runlmer.R
@@ -3,24 +3,32 @@
 library(lme4)
 #> Loading required package: Matrix
 library(lmerTest)
-#> 
-#> Attaching package: 'lmerTest'
-#> The following object is masked from 'package:lme4':
-#> 
-#>     lmer
-#> The following object is masked from 'package:stats':
-#> 
-#>     step
+library(performance)
+library (texreg) #Helps us make tables of the mixed models
+library (afex) # Easy ANOVA package to compare model fits
+library (plyr) # Data manipulator package
+library (ggplot2)
+
 library(extraoperators)
 library(JWileymisc)
 library(multilevelTools)
 
-dfuse <- read.csv("dfuse.csv")
+dfuse <- read.csv("dfuse.csv") 
 dfcatuse <- read.csv("dfcat_use.csv")
 
 df <- read.csv("df.csv")
 dfcat <- read.csv("dfcat.csv")
+#run an anova to figure out the best variables to used for the random effects
+nullmodel1 <- lmer( realRelReleaseTimes ~ 1 + (1|ferret), data = df, REML=FALSE)
+nullmodel2 <- lmer( realRelReleaseTimes ~ 1 + (1 + timeToTarget |ferret), data = df, REML=FALSE)
+nullmodel3 <- lmer( realRelReleaseTimes ~ 1 + (1 + side* timeToTarget |ferret), data = df, REML=FALSE)
+nullmodel4 <- lmer( realRelReleaseTimes ~ 1 + (1 + side* timeToTarget*AM |ferret), data = df, REML=FALSE)
+nullmodel5 <- lmer( realRelReleaseTimes ~ 1 + (1 + side+ timeToTarget+AM |ferret), data = df, REML=FALSE)
+
+anova (nullmodel1, nullmodel2, nullmodel3, nullmodel4, nullmodel5)
 
 modelreg_reduc <- lmer(
-  "realRelReleaseTimes ~ talker*(pitchoftarg)+side + talker*stepval+timeToTarget  + (1|ferret)",
-  data=dfuse, family='gamma')
+  realRelReleaseTimes ~ pitchoftarg+stepval + talker + side+ (1 + side* timeToTarget |ferret),
+  data=df, REML = FALSE)
+
+r2coeff <-r2_nakagawa(modelreg_reduc)
