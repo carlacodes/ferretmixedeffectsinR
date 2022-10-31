1d6d9f2783594f0d8efb31b33b3fa19d5a0e195d
carlacodes <carla.griffiths@gmail.com>
2022-10-31 17:58:36 +0000

    changing random intercepts

--- runlmer.R
+++ runlmer.R
@@ -33,34 +33,50 @@ nullmodel4 <- lmer( realRelReleaseTimes ~ 1 + (1 + talker+side+timeToTarget |fer
 nullmodel5 <- lmer( realRelReleaseTimes ~ 1 + (1 + talker+side+timeToTarge+AM |ferret), data = df, REML=FALSE)
 nullmodel6 <- lmer( realRelReleaseTimes ~ 1 + (1 + side+ timeToTarget+AM+DaysSinceStart |ferret), data = df, REML=FALSE)
 
-anova (nullmodel1, nullmodel2, nullmodel3, nullmodel4, nullmodel5, nullmodel6)
+nullmodel7 <- lmer( realRelReleaseTimes ~ 1 + (1 + talker+timeToTarget*side |ferret), data = df, REML=FALSE)
+anova (nullmodel1, nullmodel2, nullmodel3, nullmodel4, nullmodel5, nullmodel6, nullmodel7)
 
 
 
 
 
 modelreg_reduc1 <- lmer(
-  realRelReleaseTimes ~ pitchoftarg+ (1 + talker+side+timeToTarget|ferret),
-  data=df, REML = FALSE, control = lmerControl(optimizer ="Nelder_Mead"))
+  realRelReleaseTimes ~ pitchoftarg+(1 + talker+side+timeToTarget|ferret),
+  data=df, REML = FALSE, )#control = lmerControl(optimizer ="Nelder_Mead")
 
 modelreg_reduc2 <- lmer(
-  realRelReleaseTimes ~ pitchoftarg+stepval+ side+(1 + talker+side+timeToTarget |ferret),
-  data=df, REML = FALSE, control = lmerControl(optimizer ="Nelder_Mead"))
+  realRelReleaseTimes ~ pitchoftarg+stepval+(1 +talker+side+timeToTarget |ferret),
+  data=df, REML = FALSE,)
 
 modelreg_reduc3 <- lmer(
-  realRelReleaseTimes ~ pitchoftarg+stepval+talker+side+(1 + talker+side+timeToTarget |ferret),
-  data=df, REML = FALSE, control = lmerControl(optimizer ="Nelder_Mead"))
+  realRelReleaseTimes ~ pitchoftarg+stepval+talker+(1 +talker+side+timeToTarget |ferret),
+  data=df, REML = FALSE,)
 
 modelreg_reduc4 <- lmer(
-  realRelReleaseTimes ~ pitchoftarg+stepval+talker+side+timeToTarget+(1 + talker+side+timeToTarget |ferret),
-  data=df, REML = FALSE, control = lmerControl(optimizer ="Nelder_Mead"))
+  realRelReleaseTimes ~ pitchoftarg+stepval+talker+side+(1 + talker+side+timeToTarget |ferret),
+  data=df, REML = FALSE, )
 
 modelreg_reduc5 <- lmer(
-  realRelReleaseTimes ~ pitchoftarg*stepval*talker+side*timeToTarget+(1 + talker+side+timeToTarget |ferret),
-  data=df, REML = FALSE, control = lmerControl(optimizer ="Nelder_Mead"))
+  realRelReleaseTimes ~ pitchoftarg+stepval+talker+side+timeToTarget+(1 + talker+side+timeToTarget |ferret),
+  data=df, REML = FALSE)
 
-anova(modelreg_reduc1, modelreg_reduc2, modelreg_reduc3, modelreg_reduc4, modelreg_reduc5)
-# m1=lmer(realRelReleaseTimes ~ pizza + (1 + pizza + time |subject), data=pizzadata, REML = FALSE)
-# m2= lmer(mood ~ pizza + time + (1 + pizza + time |subject), data=pizzadata, REML = FALSE)
-# 
-r2coeff <-r2_nakagawa(modelreg_reduc2)
+modelreg_reduc6 <- lmer(
+  realRelReleaseTimes ~ pitchoftarg+stepval+talker+side+timeToTarget+AM+(1 + talker+side+timeToTarget |ferret),
+  data=df, REML = FALSE)
+
+modelreg_reduc7 <- lmer(
+  realRelReleaseTimes ~ pitchoftarg*stepval+talker+side+timeToTarget+(timeToTarget |ferret),
+  data=df, REML = FALSE)
+
+modelreg_reduc8 <- lmer(
+  realRelReleaseTimes ~ pitchoftarg*stepval+(1 + talker+side+timeToTarget |ferret),
+  data=df, REML = FALSE)
+modelreg_reduc9 <- lmer(
+  realRelReleaseTimes ~ pitchoftarg*stepval*talker+(1 + talker+side+timeToTarget |ferret),
+  data=df, REML = FALSE)
+
+
+anova(modelreg_reduc1, modelreg_reduc2, modelreg_reduc3, modelreg_reduc4, modelreg_reduc5, modelreg_reduc6, modelreg_reduc7, modelreg_reduc8, modelreg_reduc9)
+
+
+r2coeff <-r2(modelreg_reduc7)
