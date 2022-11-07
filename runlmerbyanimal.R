0c7042a188edcb6a984b3dbd1d7d08435220a078
carlacodes <carla.griffiths@gmail.com>
2022-11-07 10:13:49 +0000

    adding AIC for each animal model and plotting predicted vs actual results for each model

--- runlmerbyanimal.R
+++ runlmerbyanimal.R
@@ -11,6 +11,7 @@ library(JWileymisc)
 library(multilevelTools)
 library(hash)
 library(glmnet)
+library(AICcmodavg)
 data(QuickStartExample)
 x <- QuickStartExample$x
 y <- QuickStartExample$y
@@ -22,7 +23,9 @@ df <- read.csv("df.csv")
 
 dfcat <- read.csv("dfcat.csv")
 storemod <- list()
+aiclist <-list()
 r2coeff <- list()
+anovalist <- list()
 df$side=factor(df$side)
 df$talker=factor(df$talker)
 df$pitchoftarg=factor(df$pitchoftarg)
@@ -40,17 +43,32 @@ for (i in 0:3) {
   #pitchoftarg*talker*stepval*side*timeToTarget*trialNum+pastcorrectresp*pastcatchtrial
   df_animal <- subset(df, ferret == i)
   #df_animal <- df_animal[c( "realRelReleaseTimes", "pitchoftarg","talker","stepval", "side", "timeToTarget", "trialNum", "pastcorrectresp", "pastcatchtrial")]
- # y ~ (.)^2
-  # modelreg_reduc5 <- glm(
-    # realRelReleaseTimes  ~ pitchoftarg*talker*stepval*side*timeToTarget*trialNum*pastcorrectresp*pastcatchtrial*AM*DaysSinceStart,
-    # data=df_animal, family = 'gaussian')
+
   ##pitch of targ is highly correlated with talker identity
-  modelreg_reduc5<- glm(realRelReleaseTimes ~  pitchoftarg*talker*stepval*side*timeToTarget*trialNum*pastcorrectresp*pastcatchtrial*AM*DaysSinceStart, data=df_animal, family = 'gaussian')
-    
-  storemod[i+1] = modelreg_reduc5
-  r2coeff[i+1] = r2(modelreg_reduc5)
-  print(r2(modelreg_reduc5))
+  #modelreg_reduc5<- glm(realRelReleaseTimes ~  pitchoftarg*talker*stepval*side*timeToTarget*trialNum*pastcorrectresp*pastcatchtrial*AM*DaysSinceStart, data=df_animal, family = 'gaussian')
+  modelreg_reduc1<- glm(realRelReleaseTimes ~  pitchoftarg+stepval+side+timeToTarget+pastcatchtrial+pastcorrectresp+AM+DaysSinceStart, data=df_animal, family = 'Gamma')
+  modelreg_reduc2 <- glm(realRelReleaseTimes ~  pitchoftarg+stepval+side+timeToTarget+pastcatchtrial+pastcorrectresp+AM, data=df_animal, family = 'Gamma')
+  modelreg_reduc3 <- glm(realRelReleaseTimes ~  pitchoftarg+stepval+side+timeToTarget+pastcatchtrial+pastcorrectresp, data=df_animal, family = 'Gamma')
+  
+  
+  storemod[i+1] = modelreg_reduc1
+  r2coeff[i+1] = r2(modelreg_reduc3)
+ 
+  models <- list(modelreg_reduc1, modelreg_reduc2, modelreg_reduc3)
+  
+  #specify model names
+  mod.names <- c('1', '2', '3')
+  
+  #calculate AIC of each model
+  aiclist[i+1]=BIC(modelreg_reduc1, modelreg_reduc2, modelreg_reduc3)
+  print(BIC(modelreg_reduc1, modelreg_reduc2, modelreg_reduc3))
+  print(r2(modelreg_reduc3))
+  preddata=predict(modelreg_reduc3, df_animal, type='response')
+  
+  
+  plot(as.numeric(unlist(df_animal['realRelReleaseTimes'])), preddata, main="actual vs. predicted lick release times",
+       xlab="actual ", ylab="predicted ", pch=19)
+  abline(a=0, b=1)
   
 }
 
-#cor(df_animal$timeToTarget,df_animal$AM)
